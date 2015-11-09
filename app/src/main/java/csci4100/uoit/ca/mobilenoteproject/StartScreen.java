fba90b07cd25e984c0ce7ac7fbcd2ba22023ce0b
MKCY <milan.kornicer@uoit.net>
2015-11-08 23:42:36 -0500

    1st
    Made up the theme of the whole application
    use styles="@styles/layout" for consistency, made a Launch icon so its
    not that green android thing anymore
    
    use button.xml for buttons with color pallete

---
+++ app/src/main/java/csci4100/uoit/ca/mobilenoteproject/StartScreen.java
@@ -0,0 +1,26 @@
+package csci4100.uoit.ca.mobilenoteproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by mkcy on 11/8/2015.
+ */
+public class StartScreen extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_screen);
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    }
+
+}
